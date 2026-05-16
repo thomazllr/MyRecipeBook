3e7ddaf098b22f7d7778d4ee4eb204244d10100b
Guilherme Thomaz <guithomaz270@gmail.com>
2026-05-15 22:57:54 -0300

    feat: add register user account usecase

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -0,0 +1,14 @@
+﻿using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountUseCase
+{
+    public void Execute(RequestRegisterUserAccountJson request)
+    {
+        var validator = new RegisterUserAccountValidator();
+
+        validator.Validate(request);
+
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUserAccountJson>
+{
+    public RegisterUserAccountValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage("Nome não pode ser vazio");
+        RuleFor(user => user.Email).NotEmpty().WithMessage("Email não pode ser vazio");
+        RuleFor(user => user.Password).NotEmpty().WithMessage("Senha não pode ser vazio");
+        When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+        {
+            RuleFor(user => user.Email).EmailAddress().WithMessage("O email deve ser um email válido");
+        });
+    }
+}
