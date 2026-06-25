11f44b3ed4b435d2d57e1f4e293ef9f78d24f575
thomazllr <guithomaz270@gmail.com>
2026-06-25 00:08:20 -0300

    feat: add endpoint for get user profile

---
+++ src/Backend/MyRecipeBook.Api/Token/HttpContextTokenProvider.cs
@@ -0,0 +1,21 @@
+﻿using MyRecipeBook.Domain.Security.Tokens;
+
+namespace MyRecipeBook.Api.Token;
+
+internal sealed class HttpContextTokenProvider : IAccessTokenProvider
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public HttpContextTokenProvider(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+    public string GetToken()
+    {
+        var accessToken = _httpContextAccessor
+            .HttpContext?
+            .Request.Headers["Authorization"].ToString();
+
+        return accessToken!["Bearer ".Length..];
+    }
+}
