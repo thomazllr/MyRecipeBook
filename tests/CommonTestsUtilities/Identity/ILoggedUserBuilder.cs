80b0a16d5986dcb57549bb246c9dbc2ed5525404
thomazllr <guithomaz270@gmail.com>
2026-06-25 08:02:51 -0300

    tests; add unit tests for get user profile usecase

---
+++ tests/CommonTestsUtilities/Identity/ILoggedUserBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Identity;
+
+namespace CommonTestsUtilities.Identity;
+
+public class ILoggedUserBuilder
+{
+    public static ILoggedUser Build(User user)
+    {
+        var moq = new Mock<ILoggedUser>();
+
+        moq.Setup(loggedUser => loggedUser.Get()).ReturnsAsync(user);
+
+        moq.Setup(loggedUser => loggedUser.GetUserId()).Returns(user.Id);
+
+        return moq.Object;
+    }
+}
---
+++ tests/UseCases.Tests/User/Profile/GetUserPorfileUseCaseTests.cs
@@ -0,0 +1,32 @@
+﻿using CommonTestsUtilities.Entities;
+using CommonTestsUtilities.Identity;
+using MyRecipeBook.Application.UseCases.User.Profile;
+using Shouldly;
+
+namespace UseCases.Tests.User.Profile;
+
+public class GetUserPorfileUseCaseTests
+{
+
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var useCase = CreateUseCase(user);
+
+        var result = await useCase.Execute();
+
+        result.ShouldNotBeNull();
+        result.Name.ShouldBe(user.Name);
+        result.Email.ShouldBe(user.Email);
+    
+    }
+
+
+    private static GetUserProfileUseCase CreateUseCase(MyRecipeBook.Domain.Entities.User user)
+    {
+        var loggedUser = ILoggedUserBuilder.Build(user);
+        return new GetUserProfileUseCase(loggedUser);
+    }
+}
